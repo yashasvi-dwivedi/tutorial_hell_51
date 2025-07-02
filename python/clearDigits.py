4846511c0a17b717df759278b29d69fd661ded23
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-07-02 18:40:24 +0530

    daily

---
+++ python/clearDigits.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def clearDigits(self, s):
+        res = ""
+        for c in s:
+            if c.isdigit():
+                if res:
+                    res = res[:-1]
+            else:
+                res += c
+        return res
+
+
+# Test cases
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.clearDigits("abc123def456"))
+    print(sol.clearDigits("hello123world"))
+    print(sol.clearDigits("cb34"))
