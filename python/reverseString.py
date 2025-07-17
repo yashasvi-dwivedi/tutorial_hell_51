13adc1bdae937d3d9f3087cb38f14a9caa26e061
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-07-17 20:21:24 +0530

    reverseString

---
+++ python/reverseString.py
@@ -0,0 +1,10 @@
+def reverseString(s):
+    return s[::-1]
+
+
+# Test cases
+if __name__ == "__main__":
+    print(reverseString("hello"))  # Output: "olleh"
+    print(reverseString("Python"))  # Output: "nohtyP"
+    print(reverseString(""))  # Output: ""
+    print(reverseString("wow bob wow"))
